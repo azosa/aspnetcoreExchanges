008cccc83f13517e38d1dca8aa953d0e10f732d9
azosa <40138353+azosa@users.noreply.github.com>
2020-10-11 14:23:40 +0200

    Add project files.

---
+++ WebApplication1/Controllers/ExchangesController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class ExchangesController : Controller
+    {
+        [HttpGet]
+        public IActionResult Display()
+        {
+           
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Display(CompanyModel company)
+        {
+            var viewModel = new CompanyAddedViewModel
+            {
+                NumberOfCharsInName = company.Name.Length,
+                NumberOfCharsInDescription = company.Description.Length,
+                IsHidden = !company.IsVisible
+            };
+            return View("CompanyAdded", viewModel);
+        }
+    }
+}
